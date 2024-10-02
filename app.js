27c0e1aeba436b233ea8270114b448745e7ff4ba
Megharaj <megharajtarnalli@gmail.com>
2024-10-02 13:47:37 +0530

    updated right and left btns

--- app.js
+++ app.js
@@ -13,6 +13,8 @@
 
 //Testing below!!!!!!!!!!!!!!!!!!!!!!!!!!
 let brake_but= document.querySelector('.button_top');
+let right_but=document.querySelector(".button_right");
+let left_but=document.querySelector(".button_left");
 
 let currentLane = 0;
 const maxLanes = 3;
@@ -27,6 +29,7 @@ window.addEventListener('keydown', function (event) {
     } 
 
     else if ((event.code == "KeyD" || event.code == "ArrowRight") && currentLane < maxLanes) {
+        
         currentLane++;
     }
 
@@ -61,7 +64,31 @@ brake_but.addEventListener('mousedown', (event) =>{
 brake_but.addEventListener('mouseup', (event) =>{
     let road_img=document.querySelector('.road_img');
     road_img.style.animationDuration="1600s";
-
-
 })
+
+//right button
+right_but.addEventListener("mousedown",(event)=>{
+    if(currentLane < maxLanes){
+    currentLane++;
+    // Calculate translateX percentage based on the current lane
+    let translateXPercent = 85 + (currentLane * 70);  // Adjust this value based on spacing
+
+    // Update car's transform with new translateX based on current lane
+    car.style.transform = `scale(0.35) translateY(170%) translateX(${translateXPercent}%)`;
+    }
+})
+
+//left button
+left_but.addEventListener("mousedown",(event)=>{
+    if(currentLane > 0){
+    currentLane--;
+    // Calculate translateX percentage based on the current lane
+    let translateXPercent = 85 + (currentLane * 70);  // Adjust this value based on spacing
+
+    // Update car's transform with new translateX based on current lane
+    car.style.transform = `scale(0.35) translateY(170%) translateX(${translateXPercent}%)`;
+    }
+})
+
+
 
